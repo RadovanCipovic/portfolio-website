0fd48db7540e455159b2efb95e0ae2456261f247
radovan <Korisnik@PGFORAS.(none)>
2025-02-20 04:56:20 +0100

    JS update scrollReveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
-
 function myMenuFunction() {
   var menuBtn = document.getElementById("myNavMenu");
   if (menuBtn.className === "nav-menu") {
@@ -8,16 +7,12 @@ function myMenuFunction() {
     menuBtn.className = "nav-menu";
   }
 }
-
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-
 window.onscroll = function () {
   headerShadow();
 };
-
 function headerShadow() {
   const navHeader = document.getElementById("header");
-
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
     navHeader.style.height = "70px";
@@ -28,21 +23,33 @@ function headerShadow() {
     navHeader.style.lineHeight = "90px";
   }
 }
-
 /* ----- TYPING EFFECT ----- */
-
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-
-/* -- HOME -- */
-
-/* -- PROJECT BOX -- */
-
-/* -- HEADINGS -- */
-
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
-/* -- ABOUT INFO & CONTACT INFO -- */
-
-/* -- ABOUT SKILLS & FORM BOX -- */
+var typingEffect = new Typed(".typedText", {
+  strings: ["Junior web Developer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+});
 
 /* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll("section[id]");
+function scrollActive() {
+  const scrollY = window.scrollY;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,45 @@
+setTimeout(function () {
+  window.scrollTo(0, 1);
+}, 200);
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+/* -- HOME -- */
+sr.reveal(".featured-text-card", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 400 });
+
+// ---------------- PROJECT BOX ----------------
+sr.reveal(".project-box", { interval: 200 });
+
+/* -- HEADINGS -- */
+sr.reveal(".top-header", {});
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+
+sr.reveal(".about-info", {
+  origin: "left",
+  distance: "80px",
+  delay: 100,
+  duration: 2000,
+  reset: true,
+});
+sr.reveal(".skills-box", {
+  origin: "right",
+  distance: "80px",
+  delay: 100,
+  duration: 2000,
+  reset: true,
+});
+
+srRight.reveal(".contact-info", {});
+
+// sr.reveal(".skills-box", { delay: 100 });
+sr.reveal(".form-control", { delay: 100 });
