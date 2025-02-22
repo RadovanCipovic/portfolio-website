e34fa35f4124c5051d4e5c995eb656c463019d0c
radovan <Korisnik@PGFORAS.(none)>
2025-02-22 21:09:28 +0100

    update1

--- assets/js/download-pdf.js
+++ assets/js/download-pdf.js
@@ -1,13 +1,3 @@
-// document.getElementById("downloadBtn").addEventListener("click", function () {
-//   const pdfUrl = "https://sapphire-arabelle-30.tiiny.site";
-//   const a = document.createElement("a");
-//   a.href = pdfUrl;
-//   a.download = pdfUrl.split("/").pop();
-//   document.body.appendChild(a);
-//   a.click();
-//   document.body.removeChild(a);
-// });
-
 function pdfDownload() {
   const pdfUrl = "https://sapphire-arabelle-30.tiiny.site";
   const a = document.createElement("a");
